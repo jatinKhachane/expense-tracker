c08c22668b6d24cb3eccd1de50e122624e9ec89a
Jatin Khachane <jatin.khachane.1997@gmail.com>
2023-05-24 17:55:25 +0530

    basic jsx and css to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return (
-     <h2>Expense</h2>
-    )
-};
+  return (
+    <div className="expense-item">
+      <div>May 2023</div>
+      <div className="expense-item__description">
+        <h2>Mobile Recharge</h2>
+        <div className="expense-item__price">Rs 200</div>
+      </div>
+    </div>
+  );
+}
 
 export default ExpenseItem;
