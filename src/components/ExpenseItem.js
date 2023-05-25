bac34b58fb2302425eff73dc2afe4bed4592f111
Jatin Khachane <jatin.khachane.1997@gmail.com>
2023-05-25 17:30:01 +0530

    first use of useState to re-render component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,20 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
   const clickHandler = () => {
-    console.log("Clicked");
+    setTitle("Updated !!");
   };
 
   return (
     <Card class="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">Rs {props.amount}</div>
       </div>
       <button onClick={clickHandler}>Update Title</button>
