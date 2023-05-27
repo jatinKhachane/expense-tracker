556f00611ada90ddf94711f0d0be23e9eeb48e79
Jatin Khachane <jatin.khachane.1997@gmail.com>
2023-05-27 16:18:10 +0530

    conditional content display

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -15,9 +15,22 @@ function Expenses(props) {
   //filter the expenses -- note don't filter the original props.item which is coming from Apps.js
   //.filter -- It doesn't alters the original array
   //we dont want to update the actual list just because filtering
-  const filteredList = props.items.filter(
+  const filteredExpenses = props.items.filter(
     (item) => item.date.getFullYear().toString() === filteredYear
   );
+
+  let expeneseContent = <p>No Expenses Found!!</p>;
+
+  if (filteredExpenses.length > 0) {
+    expeneseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
 
   return (
     <div className="expenses">
@@ -26,16 +39,8 @@ function Expenses(props) {
         onSaveCurrentFilterYear={saveCurrentFilterYear}
       />
       {/* rendering the expenses data dynamically -- adding Expense react component per expense object in JSX */}
-      <Card className="expenses">
-        {filteredList.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-      </Card>
+      <Card className="expenses">{}</Card>
+      {expeneseContent}
     </div>
   );
 }
