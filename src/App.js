bd8d3ed4b8f4c21d988b069aaa19a71d25e0cc7b
Jatin Khachane <jatin.khachane.1997@gmail.com>
2023-05-27 14:27:24 +0530

    adding new expense

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const dummy_expenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
+const App = () => {
+  const [expenses, setExpsense] = useState(dummy_expenses);
+
+  // why to user function here in setExpense -- state update is dependent on prevstate hence
   const addNewExpense = (expense) => {
-    console.log("In <App/>", expense);
+    setExpsense((prevState) => {
+      return [expense, ...prevState];
+    });
   };
 
   return (
